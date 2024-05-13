098b66e77a1060cf181cc16483a87b988d55490c
Prerak <pandyaprerak4@gmail.com>
2024-05-13 15:44:15 +0530

    first commit

---
+++ Health-care-for-Android-master/app/src/main/java/com/SoftwareEng/RishitReddyMuthyala/easyHealth/PlayerConfig.java
@@ -0,0 +1,9 @@
+package com.SoftwareEng.RishitReddyMuthyala.HealthCare;
+
+/*Function to play the youtube video in the dashboard*/
+public class PlayerConfig {
+
+    public static final String YOUTUBE_API_KEY="AIzaSyC3RBUWWf9XAX9maKBca_YgVKWLSjwVITk";
+
+
+}
